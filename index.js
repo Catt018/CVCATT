462dc4c5c0268c4155021953dee8e5a596acfcc9
Caterin Bernal <cattybernal777@gmail.com>
2024-02-29 00:31:55 -0600

    catt version 1

---
+++ index.js
@@ -0,0 +1,10 @@
+const navtoggle=document.querySelector 
+(".toggle");
+
+const navmenu=document.querySelect(".nav-menu");
+
+navtoggle.addEverfListener("click",() =>{
+
+
+    nav.Menu.classList.toggle ("nav-menu;")
+});
